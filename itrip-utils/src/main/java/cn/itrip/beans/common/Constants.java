147556f5ca43ef5c5376ee2464cf74282c5521e1
wxl <weixiaoliang2491@126.com>
2020-05-14 20:26:11 +0800

    2020-05-04

---
+++ itrip-utils/src/main/java/cn/itrip/beans/common/Constants.java
@@ -0,0 +1,15 @@
+package cn.itrip.beans.common;
+
+/***
+ * 常量类 放置一些常量
+ */
+public class Constants {
+
+    //默认起始页
+    public static final Integer DEFAULT_PAGE_NO = 1;
+    //默认页大小
+    public static final Integer DEFAULT_PAGE_SIZE = 10;
+
+    //手机注册验证码前缀
+    public static final String POHONE_SMS_ACTIVE_PREFIX="activation:";
+}
---
+++ itrip-utils/src/main/java/cn/itrip/beans/common/UrlUtils.java
@@ -0,0 +1,60 @@
+package cn.itrip.beans.common;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 本类提供了对URL所指向的内容的加载操作
+ * @author hduser
+ *
+ */
+public class UrlUtils {
+
+	/**
+	 * 获取url网址返回的数据内容
+	 * @param urlStr
+	 * @return
+	 */
+	public static String loadURL(String urlStr){
+		try{  
+	        URL url = new URL(urlStr);  
+	        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();	              
+	        urlConnection.setRequestMethod("GET");  
+		    urlConnection.connect(); 	          
+		    InputStream inputStream = urlConnection.getInputStream(); 
+		    String responseStr = ConvertToString(inputStream);  
+		    return responseStr;
+		}catch(IOException e){  
+		    e.printStackTrace(); 
+		    return null;
+		}
+	}
+	private static String ConvertToString(InputStream inputStream) throws UnsupportedEncodingException {
+	    InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
+	    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  
+	    StringBuilder result = new StringBuilder();  
+	    String line = null;  
+	    try {  
+	        while((line = bufferedReader.readLine()) != null){  
+	            result.append(line + "\n");  
+	        }  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    } finally {  
+	        try{  
+	            inputStreamReader.close();  
+	            inputStream.close();  
+	            bufferedReader.close();  
+	        }catch(IOException e){  
+	            e.printStackTrace();  
+	        }  
+	    }  
+	    return result.toString();  
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperties().getProperty("file.encoding"));
+	}
+}
